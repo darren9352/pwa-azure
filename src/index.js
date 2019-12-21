5384d73596a9b7ecdb66895691f0691c2cac2448
darren9352 <darren9352@gmail.com>
2019-12-21 15:28:10 +0900

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+// https://github.com/techhtml/2019-12-21-pwa-workshop
+const App = () =>(
+    <div className ="app">
+        Hello Azure world
+    </div>
+)
+
+ReactDOM.render(<App />, document.getElementById("app"))
